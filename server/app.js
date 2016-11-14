d3b31d045f058a1d29c5a093c7ddc21b23e56556
Steve Larsen <stevelarsen01@gmail.com>
2016-11-14 15:24:16 -0600

    add CSS styling

--- server/app.js
+++ server/app.js
@@ -4,37 +4,35 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 // routing modules
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // initial jokes provided by the client
-var jokes = [
-  {
+var jokes = [{
     whoseJoke: "Huck",
     jokeQuestion: "What's the difference between roast beef and pea soup?",
     punchLine: "Anyone can roast beef."
-  },
-  {
+}, {
     whoseJoke: "Kris",
     jokeQuestion: "How many software engineers does it take to change a lightbulb?",
     punchLine: "None! That's a hardware problem!"
-  },
-  {
+}, {
     whoseJoke: "Luke",
     jokeQuestion: "Friends are like snow flakes...",
     punchLine: "If you pee on them they disappear."
-  }
-];
+}];
 
 app.get('/jokes', function(req, res) {
-  console.log('get /');
-  res.send(jokes);
+    console.log('get /');
+    res.send(jokes);
 });
 
 app.post('/joke', function(req, res) {
-  jokes.push(req.body);
-  console.log(jokes);
-  console.log('post /');
-  res.sendStatus(201);
+    jokes.push(req.body);
+    console.log(jokes);
+    console.log('post /');
+    res.sendStatus(201);
 });
 
 // static files
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,62 +3,76 @@ console.log('js loaded');
 $(document).ready(function() {
     console.log("document ready");
 
+    // get and display jokes on DOM
     getJokes();
 
+    // =====  EVENTS  ======
+    // Add joke event
     $('#addButton').on('click', function() {
-      console.log('add button clicked');
-      addJoke();
-
+        addJoke();
     });
 
+    // ===== FUNCTIONS  =======
+
     function addJoke() {
-      var joke = {
-        whoseJoke: $('#whoseJoke').val(),
-        jokeQuestion: $('#jokeQuestion').val(),
-        punchLine: $('#punchLine').val()
-      };
-      console.log(joke);
-      putJoke(joke);
-      $('.jokes').empty();
-      getJokes();
+        var joke = {
+            whoseJoke: $('#whoseJoke').val(),
+            jokeQuestion: $('#jokeQuestion').val(),
+            punchLine: $('#punchLine').val()
+        };
+        putJoke(joke);
+        clearDisplay();
+        getJokes();
     } // end addJoke
 
+    // put joke on server
     function putJoke(joke) {
-      $.ajax({
-          type: 'POST',
-          url: '/joke',
-          data: joke,
-          success: function(data) {
-            console.log("Success - POST");
-          },
-          error: function(){
-            console.log("Error - POST");
-          }
-      });
+        $.ajax({
+            type: 'POST',
+            url: '/joke',
+            data: joke,
+            success: function(data) {
+                console.log("Success - POST");
+            },
+            error: function() {
+                console.log("Error - POST");
+            }
+        });
     } // end putJoke
 
+    // get jokes from server
     function getJokes() {
-      $.ajax({
-          type: 'GET',
-          url: '/jokes',
-          success: function(data) {
-            console.log(data);
-            appendJokes(data);
-            console.log("Success - GET");
-          },
-          error: function(){
-            console.log("Error - GET");
-          }
-      });
+        $.ajax({
+            type: 'GET',
+            url: '/jokes',
+            success: function(data) {
+                appendJokes(data);
+                console.log("Success - GET");
+            },
+            error: function() {
+                console.log("Error - GET");
+            }
+        });
     } // end getJokes
 
+    // append all jokes to DOM
     function appendJokes(jokes) {
-      for (var i = 0; i < jokes.length; i++) {
-        var string = '<p>Q: ' + jokes[i].jokeQuestion + '</p>';
-        string += '<p>A: ' + jokes[i].punchLine + '</p>';
-        string += '<p>Contributed By: ' + jokes[i].whoseJoke + '</p>';
-        $('.jokes').append(string);
-      }
+        for (var i = 0; i < jokes.length; i++) {
+            var string = '<div class="joke">';
+            string += '<p class="jokeQuestion">Q: ' + jokes[i].jokeQuestion + '</p>';
+            string += '<p class="punchLine">A: ' + jokes[i].punchLine + '</p>';
+            string += '<p class="whoseJoke">Contributed By: ' + jokes[i].whoseJoke + '</p>';
+            string += '</div>';
+            $('.jokes').append(string);
+        }
     } // end appendJokes
 
-});  // end document ready
+    // clears entry fields and joke display
+    function clearDisplay() {
+        $('.jokes').empty();
+        $('#whoseJoke').val('');
+        $('#jokeQuestion').val('');
+        $('#punchLine').val('');
+    } // end of clearDisplay
+
+}); // end document ready
